7a67bea42d7802278c6b5e772c36e40b586e065d
Daksh88934 <dakshbaraliya2006@gmail.com>
2026-03-17 15:02:38 +0530

    feat(landing): update tagline to 'Building the future with AI, ML, and code.'

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -15,14 +15,9 @@ const Landing = ({ children }: PropsWithChildren) => {
             </h1>
           </div>
           <div className="landing-info">
-            <h3>An AI & ML</h3>
+            <h3>Building the future with</h3>
             <h2 className="landing-info-h2">
-              <div className="landing-h2-1">Data Scientist</div>
-              <div className="landing-h2-2">Developer</div>
-            </h2>
-            <h2>
-              <div className="landing-h2-info">Developer</div>
-              <div className="landing-h2-info-1">Data Scientist</div>
+              <div className="landing-h2-info">AI, ML, and code.</div>
             </h2>
           </div>
         </div>
